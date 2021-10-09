1824240ea5a06ee1ae283c81ade1bf8ffcb26fe0
OlgaPyryeva <vashkevicholga@gmail.com>
2021-10-09 16:25:43 +0300

    удалила лишнюю библиотеку

--- src/components/pages/main/carousel/Carousel.js
+++ src/components/pages/main/carousel/Carousel.js
@@ -2,8 +2,6 @@ import React from "react";
 import Carousel from "nuka-carousel";
 import Slaide1 from "./slides/Slaide1";
 import "./Carousel.css";
-import catBtn from "./img/catBtn.svg";
-import catBtn1 from "./img/catBtn1.svg";
 
 export default class extends React.Component {
   state = {
