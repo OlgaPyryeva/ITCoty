09e602646022fc38de5d3be76e6f0fc6e981c796
OlgaPyryeva <vashkevicholga@gmail.com>
2021-10-13 19:09:17 +0300

    верстка Авторизации

--- src/components/socialNetworkReg/SocialNetworkReg.js
+++ src/components/socialNetworkReg/SocialNetworkReg.js
@@ -1,32 +1,39 @@
 import React from "react";
-import facebook from "../socialNetworkReg/img/facebook.svg";
-import google from "../socialNetworkReg/img/google.svg";
-import linkedIn from "../socialNetworkReg/img/linkedIn.svg";
+import facebook from "../img/facebook.svg";
+import google from "../img/google.svg";
+import linkedin from "../img/linkedin.svg";
+import "./SocialNetworkReg.css";
 
 export default function socialNetworkReg() {
   return (
-    <div>
-      <button className="socialIconBtn">
-        <img
-          src={facebook}
-          alt="facebook logo"
-          title="Продолжить через facebook"
-        />
-      </button>
-      <button className="socialIconBtn">
-        <img
-          src={linkedIn}
-          alt="linkedin logo"
-          title="Продолжить через linkedin"
-        />
-      </button>
-      <button className="socialIconBtn">
-        <img
-          src={google}
-          alt="facebook logo"
-          title="Продолжить через facebook"
-        />
-      </button>
+    <div className="socialIconWrapper">
+      <div className="socialIconContainer">
+        <div className="socialIconBtn">
+          <button className="socialBtn">
+            <div> Продолжить через</div>
+            <div>
+              <img src={facebook} alt="facebook logo" />
+            </div>
+          </button>
+        </div>
+        <div className="socialIconBtn">
+          <button className="socialBtn">
+            <div>Продолжить через</div>
+            <div>
+              {" "}
+              <img src={linkedin} alt="linkedin logo" />
+            </div>
+          </button>
+        </div>
+        <div className="socialIconBtn">
+          <button className="socialBtn">
+            <div> Продолжить через</div>{" "}
+            <div>
+              <img src={google} alt="facebook logo" />
+            </div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
